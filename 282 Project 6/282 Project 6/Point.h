c15d0d685d85cb14ec1a36f2be26a48e34380aa5
ijosepham <44876783+ijosepham@users.noreply.github.com>
2019-04-12 16:59:14 -0700

    filled in the header file with setters, getters, overloaded operators for cin/cout. also made class point of type T

--- 282 Project 6/282 Project 6/Point.h
+++ 282 Project 6/282 Project 6/Point.h
@@ -1,8 +1,34 @@
+// Point.h
+///////////
+
 #pragma once
+#include <iostream>
+
+using namespace std;
+
+template < class T >
 class Point
 {
+private:
+	T x;
+	T y;
 public:
+	// constructors
 	Point();
+	Point(T, T);
+
+	// setters
+	void setX(T);
+	void setY(T);
+	
+	// getters
+	T getX();
+	T getY();
+
+	// friends cin/cout
+	friend ostream& operator<<(ostream&, Point&);
+	friend istream& operator>>(istream&, Point&);
+
 	~Point();
 };
 
